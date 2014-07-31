97406fb48a9b4d907db0f1d1db3e84d1c2dd5bfb
Jonadab the Unsightly One <jonadab@columbus.rr.com>
2014-07-31 08:04:49 -0400

    Fix !seen to correctly report timezones when $servertz is not "UTC".

--- jonadabot_guts.pl
+++ jonadabot_guts.pl
@@ -965,7 +965,7 @@ sub handlemessage {
     } elsif (@s) {
       my $s = $s[0];
       $answer = "/me last saw $$s{nick} in $$s{channel} "
-        . friendlytime(DateTime::Format::MySQL->parse_datetime($$s{whenseen}),
+        . friendlytime(DateTime::Format::MySQL->parse_datetime($$s{whenseen}->set_timezone($servertz)),
                        getircuserpref($sender, 'timezone')) . ".";
     }
     if (($howtorespond eq 'private') or ($irc{okdom}{$howtorespond})) {
